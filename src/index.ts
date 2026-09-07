ebe88fee798bcfe4c4f101a778f8a3863ab3e918
arranHarty <35041824+arranHarty123@users.noreply.github.com>
2026-09-07 15:47:07 -0700

    Scaffold TypeScript plein check (happy path)

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export {
+  checkPlein,
+  parsePlein,
+  ParseError,
+  type PleinModel,
+  type ElementDecl,
+  type RelationshipDecl,
+} from "./parser.js";
+export { ELEMENT_KEYWORDS, RELATIONSHIP_KEYWORDS } from "./keywords.js";
